af3064e031f46a862a08ac68f3f68f7f3f5c5587
Mmeli <mmeligabrieldyantyi@gmail.com>
2026-01-21 15:57:45 +0200

    Setup CI/CD pipeline

--- backend/server.js
+++ backend/server.js
@@ -176,3 +176,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} \n http://localhost:${PORT}/health/ready`);
 });
+console.log(`Server running on port ${PORT} - Deployed via GitHub Actions!`);
